d343256963043afdd84498e7877593be2dd30547
Dimasprmh <166973858+Dimasprmh@users.noreply.github.com>
2024-11-28 21:52:11 +0700

    Delete md/HomePage directory

--- md/HomePage/app/src/main/java/com/example/homepage/data/TrendingData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.homepage.data
-
-data class Trending(
-    val title: String,
-    val imageResId: Int,
-    val votes: String
-)
--- md/HomePage/app/src/main/java/com/example/homepage/data/data.kt
+++
@@ -1,6 +0,0 @@
-package com.example.homepage.data
-
-data class Category(
-    val name: String,
-    val imageResId: Int // Resource ID untuk gambar kategori
-)
